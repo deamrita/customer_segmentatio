7d2a037b5526e589088c07ac9f290f91485e65d6
Amrita De <deamrita2015@gmail.com>
2022-03-20 19:30:10 +0530

    Add files via upload

---
+++ Project 3 Customer segmentation with k means clustering.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# In[2]:
+
+
+df = pd.read_csv("C:/Users/Amrita/OneDrive/Desktop/Capstone_project/Mall_Customers.csv")
+df.head()
+
+
+# In[3]:
+
+
+#dropped the ID column
+df.drop(["CustomerID"], axis = 1, inplace=True)
+
+
+# In[4]:
+
+
+#Ages Frequency violinplot
+plt.figure(figsize=(10,6))
+plt.title("Ages Frequency")
+sns.axes_style("dark")
+sns.violinplot(y=df["Age"])
+plt.show()
+
+
+# In[5]:
+
+
+#box plot of Spending Score and Annual Income
+plt.figure(figsize=(15,6))
+plt.subplot(1,2,1)
+sns.boxplot(y=df["Spending Score (1-100)"], color="red")
+plt.subplot(1,2,2)
+sns.boxplot(y=df["Annual Income (k$)"])
+plt.show()
+
+
+# In[6]:
+
+
+#bar plot of gender
+genders = df.Gender.value_counts()
+sns.set_style("darkgrid")
+plt.figure(figsize=(10,4))
+sns.barplot(x=genders.index, y=genders.values)
+plt.show()
+
+
+# In[7]:
+
+
+#age group
+age18_25 = df.Age[(df.Age <= 25) & (df.Age >= 18)]
+age26_35 = df.Age[(df.Age <= 35) & (df.Age >= 26)]
+age36_45 = df.Age[(df.Age <= 45) & (df.Age >= 36)]
+age46_55 = df.Age[(df.Age <= 55) & (df.Age >= 46)]
+age55above = df.Age[df.Age >= 56]
+
+x = ["18-25","26-35","36-45","46-55","55+"]
+y = [len(age18_25.values),len(age26_35.values),len(age36_45.values),len(age46_55.values),len(age55above.values)]
+
+plt.figure(figsize=(15,6))
+sns.barplot(x=x, y=y, palette="rocket")
+plt.title("Number of Customer and Ages")
+plt.xlabel("Age")
+plt.ylabel("Number of Customer")
+plt.show()
+
+
+# In[8]:
+
+
+#spending score
+ss1_20 = df["Spending Score (1-100)"][(df["Spending Score (1-100)"] >= 1) & (df["Spending Score (1-100)"] <= 20)]
+ss21_40 = df["Spending Score (1-100)"][(df["Spending Score (1-100)"] >= 21) & (df["Spending Score (1-100)"] <= 40)]
+ss41_60 = df["Spending Score (1-100)"][(df["Spending Score (1-100)"] >= 41) & (df["Spending Score (1-100)"] <= 60)]
+ss61_80 = df["Spending Score (1-100)"][(df["Spending Score (1-100)"] >= 61) & (df["Spending Score (1-100)"] <= 80)]
+ss81_100 = df["Spending Score (1-100)"][(df["Spending Score (1-100)"] >= 81) & (df["Spending Score (1-100)"] <= 100)]
+
+ssx = ["1-20", "21-40", "41-60", "61-80", "81-100"]
+ssy = [len(ss1_20.values), len(ss21_40.values), len(ss41_60.values), len(ss61_80.values), len(ss81_100.values)]
+
+plt.figure(figsize=(15,6))
+sns.barplot(x=ssx, y=ssy, palette="nipy_spectral_r")
+plt.title("Spending Scores")
+plt.xlabel("Score")
+plt.ylabel("Number of Customer Having the Score")
+plt.show()
+
+
+# In[9]:
+
+
+#annual income
+ai0_30 = df["Annual Income (k$)"][(df["Annual Income (k$)"] >= 0) & (df["Annual Income (k$)"] <= 30)]
+ai31_60 = df["Annual Income (k$)"][(df["Annual Income (k$)"] >= 31) & (df["Annual Income (k$)"] <= 60)]
+ai61_90 = df["Annual Income (k$)"][(df["Annual Income (k$)"] >= 61) & (df["Annual Income (k$)"] <= 90)]
+ai91_120 = df["Annual Income (k$)"][(df["Annual Income (k$)"] >= 91) & (df["Annual Income (k$)"] <= 120)]
+ai121_150 = df["Annual Income (k$)"][(df["Annual Income (k$)"] >= 121) & (df["Annual Income (k$)"] <= 150)]
+
+aix = ["$ 0 - 30,000", "$ 30,001 - 60,000", "$ 60,001 - 90,000", "$ 90,001 - 120,000", "$ 120,001 - 150,000"]
+aiy = [len(ai0_30.values), len(ai31_60.values), len(ai61_90.values), len(ai91_120.values), len(ai121_150.values)]
+
+plt.figure(figsize=(15,6))
+sns.barplot(x=aix, y=aiy, palette="Set2")
+plt.title("Annual Incomes")
+plt.xlabel("Income")
+plt.ylabel("Number of Customer")
+plt.show()
+
+
+# In[10]:
+
+
+plt.subplot(3, 1, 1); sns.distplot(df['Age'])
+
+# Plot frequency distribution
+plt.subplot(3, 1, 2); sns.distplot(df['Spending Score (1-100)'])
+
+# Plot monetary value distribution
+plt.subplot(3, 1, 3); sns.distplot(df['Annual Income (k$)'])
+
+
+# In[11]:
+
+
+#Pre-process the data (Log normalize + scale and centre)
+
+# Import StandardScaler 
+from sklearn.preprocessing import StandardScaler
+
+df1 = df[["Age","Spending Score (1-100)","Annual Income (k$)"]]
+# Unskew the data
+datamart_log = np.log(df1)
+
+# Initialize a standard scaler and fit it
+scaler = StandardScaler()
+scaler.fit(datamart_log)
+
+# Scale and center the data
+datamart_normalized = scaler.transform(datamart_log)
+
+# Create a pandas DataFrame
+datamart_normalized = pd.DataFrame(data=datamart_normalized, index=df1.index, columns=df1.columns)
+
+
+# In[12]:
+
+
+plt.subplot(3, 1, 1); sns.distplot(datamart_normalized['Age'])
+
+# Plot frequency distribution
+plt.subplot(3, 1, 2); sns.distplot(datamart_normalized['Spending Score (1-100)'])
+
+# Plot monetary value distribution
+plt.subplot(3, 1, 3); sns.distplot(datamart_normalized['Annual Income (k$)'])
+
+
+# In[13]:
+
+
+from sklearn.cluster import KMeans
+wcss = []
+for k in range(1,11):
+    kmeans = KMeans(n_clusters=k, init="k-means++")
+    kmeans.fit(datamart_normalized)
+    wcss.append(kmeans.inertia_)
+plt.figure(figsize=(12,6))    
+plt.grid()
+plt.plot(range(1,11),wcss, linewidth=2, color="red", marker ="8")
+plt.xlabel("K Value")
+plt.xticks(np.arange(1,11,1))
+plt.ylabel("WCSS")
+plt.show()
+
+
+# In[15]:
+
+
+km = KMeans(n_clusters=5)
+clusters = km.fit_predict(datamart_normalized)
+df["label"] = clusters
+
+
+
+# In[16]:
+
+
+df
+
+
+# In[17]:
+
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+ 
+fig = plt.figure(figsize=(20,10))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(df.Age[df.label == 0], df["Annual Income (k$)"][df.label == 0], df["Spending Score (1-100)"][df.label == 0], c='blue', s=60)
+ax.scatter(df.Age[df.label == 1], df["Annual Income (k$)"][df.label == 1], df["Spending Score (1-100)"][df.label == 1], c='red', s=60)
+ax.scatter(df.Age[df.label == 2], df["Annual Income (k$)"][df.label == 2], df["Spending Score (1-100)"][df.label == 2], c='green', s=60)
+ax.scatter(df.Age[df.label == 3], df["Annual Income (k$)"][df.label == 3], df["Spending Score (1-100)"][df.label == 3], c='orange', s=60)
+ax.scatter(df.Age[df.label == 4], df["Annual Income (k$)"][df.label == 4], df["Spending Score (1-100)"][df.label == 4], c='purple', s=60)
+ax.view_init(30, 185)
+plt.xlabel("Age")
+plt.ylabel("Annual Income (k$)")
+ax.set_zlabel('Spending Score (1-100)')
+plt.show()
+
+
+# In[ ]:
+
+
+
+
